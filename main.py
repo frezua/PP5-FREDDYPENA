02045a200477bc4a5059a7eb3166766059fca03d
Freddy Peña <120146694+frezua@users.noreply.github.com>
2022-12-09 01:54:08 +0000

    paso #6

--- main.py
+++ main.py
@@ -4,30 +4,37 @@ Refinando Código
 Refinacion de codigo y se publicara en GITHUB
 """
 
+import sys
+
 
 def costs_list():
-    """Devolucion lista de costos"""
-    with open('gift_costs.txt', 'r', encoding='UTF-8') as archivo:
-        gift_costs = list(archivo)
+  """Devolucion lista de costos"""
+  with open('gift_costs.txt', 'r', encoding='UTF-8') as archivo:
+    gift_costs = list(archivo)
+  try:
     gift_costs = [int(c) for c in gift_costs]
     archivo.close()
-    return gift_costs
+  except ValueError:
+    print('Los datos deben ser digitos.')
+    sys.exit()
+  return gift_costs
 
 
 def total(gift_costs):
-    """Sumatoria lista de costos"""
-    precio_final = 0
-    for cost in gift_costs:
-        if cost > 1000:
-            precio_final += cost * 1.16
-        else:
-            precio_final += cost
+  """Sumatoria lista de costos"""
+  precio_final = 0
+  for cost in gift_costs:
+    if cost > 1000:
+      precio_final += cost * 1.16
+    else:
+      precio_final += cost
 
-    return precio_final
+  return precio_final
 
 
 def main():
-    """Impresion del total"""
-    print(total(costs_list()))
+  """Impresion del total"""
+  print(total(costs_list()))
+
 
 main()
