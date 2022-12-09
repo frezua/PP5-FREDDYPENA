189a791826eeb16737f1909250260d74088507cb
Freddy Peña <120146694+frezua@users.noreply.github.com>
2022-12-09 01:13:00 +0000

    paso #4

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-with open('gift_costs.txt') as f:
-    gift_costs = f.read().split('\n')
-    
-gift_costs = [int(c) for c in gift_costs]  # convierte strings a int
+"""
+Freddy Enrique Peña Tejada 23-0943
+Refinando Código
+En este ejercicio, se refinará un código dado y se publicará en un repositorio de Github
+"""
 
-total_price = 0
-for cost in gift_costs:
-    if cost > 1000:
-        total_price += cost * 1.16  # agrega impuestos
-    else:
-        total_price += cost
 
-print(total_price)
+def costos_lista():
+    archivo = open('gift_costs.txt', 'r')
+    gift_costs = list(archivo)
+    gift_costs = [int(c) for c in gift_costs]  # convierte strings a int
+    archivo.close()  # cerrar el archivo después de usarlo y no ser necesario
+    return gift_costs
+
+
+def total(gift_costs):
+    precio_final = 0
+    for cost in gift_costs:
+        if cost > 1000:
+            precio_final += cost * 1.16  # agrega impuestos
+        else:
+            precio_final += cost  # los costos menores a 1000 no se le agrega impuesto
+
+    return precio_final
+
+
+def main():
+    print(total(costos_lista()))
+    # llama a los dos funciones y luego imprime el resultado
+
+if __name__ == '_main_':
+    main()
